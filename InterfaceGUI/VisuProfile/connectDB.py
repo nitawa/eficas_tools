11f8833417b06a8af0656a6425bcfe1e021f08dc
PASCALE NOYRET <pascale.noyret@edf.fr>
2024-12-20 17:04:00 +0100

    fin chargement d un seul profile

--- InterfaceGUI/VisuProfile/connectDB.py
+++ InterfaceGUI/VisuProfile/connectDB.py
@@ -1,15 +1,14 @@
 #! /usr/bin/env python3
 # -*- coding:utf-8 -*-
 import sys
-
 import psycopg2
+
 
 connecteurTexte = "dbname=odyssee_test user=admin_odyssee_test"
 
 
 class connectDB:
     """
-    lit un fichier XML de perfomance et le charge dans la database
     code exit : 1 erreur database, 2 autres erreur
     """
 
@@ -19,6 +18,7 @@ class connectDB:
            connexion a la database
            ouverture du curseur
         """
+        if debug : print ('init du connect')
         try:
             self.connecteur = psycopg2.connect(connecteurTexte)
         except Exception as e:
@@ -26,6 +26,7 @@ class connectDB:
             print("exception", e)
             self.closeDB()
             exit(1)
+        if debug : print ('connecteur', self.connecteur)
         try:
             self.curseur = self.connecteur.cursor()
         except Exception as e:
@@ -33,32 +34,40 @@ class connectDB:
             print("exception", e)
             self.closeDB()
             exit(1)
+        if debug : print ('curseur', self.curseur)
 
     def executeSelectDB(self, instruction, debug = 0):
-    #--------------------------------------------------------#
-        if debug:
-            print("executeSelectDB avec ", instruction )
+    #------------------------------------------------#
+        if debug: print("executeSelectDB avec ", instruction )
         try:
+            if debug : print ('ici excute')
             self.curseur.execute(instruction)
-            resultat = self.curseur.fetchall() 
-            if debug : print ("resultat ", resultat)
-            return resultat
+            if debug : print ('apres excute')
+            try :
+                resultat = self.curseur.fetchall() 
+            except Exception as e:
+                print("probleme au fetch  l execution de", instruction)
+                print("exception", e)
+                return None 
         except Exception as e:
             print("probleme a l execution de", instruction)
             print("exception", e)
-            exit(1)
+            return None 
 
-    def executeInsertDB(self, instruction, valeur, debug = 0):
+    def executeDBInstruction(self, instruction, debug = 0):
     #-------------------------------------------------------#
-        if debug:
-            print("executeInsertDB avec ", instruction, " ", valeur)
+        if debug: print("executeInsertDB avec ", instruction)
         try:
-            self.curseur.execute(instruction, valeur)
+            self.curseur.execute(instruction)
+            try :
+                resultat = self.curseur.fetchall() 
+            except : 
+                resultat = None
+            if debug : print ('resultat', resultat)
+            return resultat
         except Exception as e:
             print("impossible d executer", instruction)
-            print("avec les valeurs", valeur)
             print("exception", e)
-            exit(1)
 
     def commitDB(self):
     #-----------------#
