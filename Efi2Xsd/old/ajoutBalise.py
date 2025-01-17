caa4e6f298ebe9a4d2d1e131e0d92aaa4673a0f5
PASCALE NOYRET <pascale.noyret@edf.fr>
2025-01-17 11:01:31 +0100

    nettoyage . il faut mettre une option pour l entete des balises

--- Efi2Xsd/old/ajoutBalise.py
+++
@@ -1,6 +0,0 @@
-debuttypeChaineAvecBlancs = '\t\t<xs:simpleType name="T_stg">\n\t\t\t <xs:restriction>\n\t\t\t <xs:simpleType>\n\t\t\t\t <xs:list>\n\t\t\t\t <xs:simpleType>\n\t\t\t\t\t <xs:restriction base="xs:string">'
-fintypeChaineAvecBlancs = '\t\t</xs:restriction>\n\t\t </xs:simpleType>\n\t\t </xs:list>\n\t\t </xs:simpleType>\n\t\t'
-
-SequenceChaineAvecBlancs = '\t\t<xs:complexType name="T_{}">\n\t\t\t  <xs:sequence> <xs:element name="stg" type="Vimmp:T_stg" minOccurs="1" maxOccurs="1" default="{}">n\t\t\t  <\xs:sequence>\n\t\t<xs:complexType name>\n'
-
-
